2ae2bc4c35b12a5126f5868ce799b63509ec8821
Reed Chen <reedchen19@gmail.com>
2021-12-02 23:29:00 -0500

    updated code

--- main.m
+++ main.m
@@ -3,8 +3,10 @@ clear
 %import data
 HR_image = rgb2gray(imread('./stars/Full Res Star.png')); % High-res/ground truth image
 LR_images = {}; % low-res images
+
 for i = 1:5
-    file = sprintf('./stars/Low Res Star %d.png', i)
+    %file = sprintf('./stars/Low Res Star %d.png', i);
+    file = sprintf('./Circles/Low_res Circle %d.png', i);
     LR_images{i} = abs(255-rgb2gray(imread(file)));
 end
 %imshow(LR_images{1})
@@ -21,15 +23,15 @@ ycorr = [];
 avg_MR = []; %averaged mid-res image @Advaita
 
 %Upsampling
-MR_images = resize(LR_images, x_LR*scale, y_LR*scale);
+MR_images = resize(LR_images, scale);
 figure(1)
-for i = 1:5
-    subplot(5,1,i)
+for i = 1:num_img
+    subplot(num_img,1,i)
     imshow(LR_images{i})
 end
 figure(2)
-for i = 1:5
-    subplot(5,1,i)
+for i = 1:num_img
+    subplot(num_img,1,i)
     imshow(MR_images{i})
 end
 
--- resize.m
+++ resize.m
@@ -1,33 +1,29 @@
 % Takes in cell of multiple grayscale images
 % Returns resized images by cropping/padding in fourier space.
-% Parameters: input = image cell, size_x = desired output image size in
-% x-direction, size_y = desired output image size in y-direction.
+% Parameters: input = image cell, scale = factor to scale image by.
 
-% size_x and size_y parameters should both be larger/smaller than the
-% input image dimensions, ie, if size_x > x-dimension of input image, then
-% size_y should also be greater than the y-dimension of input image and not
-% less.
-function output = resize(input, size_x, size_y)
+function output = resize(input, scale)
     imgs = {};
     for i = 1:length(input)
         [x_dim, y_dim] = size(input{i}); % get dimensions of input image
         freq = fftshift(fft2(input{i})); % take Fourier transform
-        if size_x>x_dim && size_y>y_dim % if desired image size is larger than input image, pad in Fourier space
-            x = ceil((size_x-x_dim)/2); % padding in x direction
-            y = ceil((size_y-y_dim)/2); % padding in y direction
+        if scale>=1 % if desired image size is larger than input image, pad in Fourier space
+            x = floor(x_dim*(scale-1)/2); % padding in x direction
+            y = floor(y_dim*(scale-1)/2); % padding in y direction
             freq = padarray(freq,[x y],0,'both'); % pad w/ 0s in fourier space
         end
-        if size_x<x_dim && size_y<y_dim % if the desired image is smaller than input image, crop in Fourier space
+        if scale<1 % if the desired image is smaller than input image, crop in Fourier space
             x_center = floor(x_dim/2)+1; % center of image along x direction
             y_center = floor(y_dim/2)+1; % center of image along y direction
-            crop_x = floor(size_x/2); % crop size along x direction
-            crop_y = floor(size_y/2); % crop size along y direction
+            crop_x = floor(x_dim*scale/2); % crop size along x direction
+            crop_y = floor(y_dim*scale/2); % crop size along y direction
 
-            freq = freq(x_center-crop_x:x_center+crop_x, y_center-crop_y:y_center+crop_y); % crop to keep center of image
+            freq = freq(x_center-crop_x:x_center+crop_x, y_center-crop_y:y_center+crop_y); % crop to keep low frequencies
         end
         %imshow(freq)
-        scale = size_x*size_y/(x_dim*y_dim); % scaling factor to scale Fourier transform
-        imgs{i} = uint8(real(ifft2(ifftshift(freq*scale)))); % inverse FFT back to spatial domain
+        [xo_dim, yo_dim] = size(freq); % get dimensions of output image
+        mult = xo_dim*yo_dim/(x_dim*y_dim); % scaling factor to scale Fourier transform
+        imgs{i} = uint8(real(ifft2(ifftshift(freq*mult)))); % inverse FFT back to spatial domain
     end
     output = imgs;
 end
