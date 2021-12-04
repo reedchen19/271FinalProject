1d3005acee058abc65e94bafd0a89422f3a8eac6
liu-y-andrew <94942434+liu-y-andrew@users.noreply.github.com>
2021-12-04 16:30:11 -0500

    Add files via upload

--- imgCorr.m
+++ imgCorr.m
@@ -3,20 +3,20 @@
 %returns column and row shifts (Xcorr and Ycorr) of max correlation
 %index 1 of Xcorr and Ycorr = autocorrelation of reference img
 function [Xcorr, Ycorr, max_corr, max_index, imgCorr] = imgCorr(imgs)
-    Xcorr = zeros([1 length(imgs)]);
-    Ycorr = zeros([1 length(imgs)]);
+    Xcorr = zeros([1 length(imgs)]); %X shift values (index 1 = autocorr)
+    Ycorr = zeros([1 length(imgs)]); %Y shift values (index 1 = autocorr)
         
-    Xcorr(1) = max(max(xcorr2(cell2mat(imgs(1)))));
-    Ycorr(1) = max(max(xcorr2(cell2mat(imgs(1)))));
+    Xcorr(1) = max(max(xcorr2(cell2mat(imgs(1))))); %set index 1 = autocorr
+    Ycorr(1) = max(max(xcorr2(cell2mat(imgs(1))))); %set index 1 = autocorr
         
     for i = 2:length(imgs)
-        matref = cell2mat(imgs(1));
-        matimg = cell2mat(imgs(i));
-        imgCorr = xcorr2(matimg,matref);
-        imgCorr = imgCorr.*(imgCorr < Xcorr(1));
-        [max_corr, max_index] = max(imgCorr,[],'all','linear');
-        [ypeak,xpeak] = ind2sub(size(imgCorr),max_index);
-        Xcorr(i) = xpeak - size(imgs{1},2);
-        Ycorr(i) = ypeak - size(imgs{1},1);
+        matref = cell2mat(imgs(1)); %define reference img as first img
+        matimg = cell2mat(imgs(i)); %define test img for correlating
+        imgCorr = xcorr2(matref, matimg); %perform 2d correlation
+        imgCorr = imgCorr.*(imgCorr < Xcorr(1)); %verify against autocorr
+        [max_corr, max_index] = max(imgCorr,[],'all','linear'); %find max correlation in reference
+        [ypeak,xpeak] = ind2sub(size(imgCorr),max_index); %locate y row and x column for max correlation
+        Xcorr(i) = xpeak - size(imgs{1},2); %convert x column of correlation to x shift in test img
+        Ycorr(i) = ypeak - size(imgs{1},1); %convert y row of correlation to y shift in test img
     end
 end
